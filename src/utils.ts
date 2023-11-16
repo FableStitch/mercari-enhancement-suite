475663f4e7b641c3b81f010e11fc9a1233e9fb52
matthewaerose <matthewaerose@users.noreply.github.com>
2023-11-16 20:25:11 +0000

    automatic update to

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const checkVersion: string = '1.26.363'
+export const checkVersion: string = '1.26.364'
 
 export const sleep = (delay: number) =>
   new Promise((resolve) => setTimeout(resolve, delay))
